552be7447460bc1bdcb35223755597e8dc0b3233
Tim Whelan <twhelan@dr.local>
2020-06-04 16:59:30 +0100

    adding sagas for INCREMENT and INCREMENT_ASYNC

--- sagas.js
+++ sagas.js
@@ -1,3 +1,31 @@
-export function* helloSaga() {
-  console.log("hello sagas!");
+import { put, takeEvery } from "redux-saga/effects";
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+function* helloSaga() {
+  console.log("Hello Sagas!");
+}
+
+// Our worker Saga: will perform the async increment task
+export function* incrementAsync() {
+  yield delay(1000);
+
+  // instructs the middleware to schedule
+  // the dispatching of an action to the store.
+  yield put({ type: "INCREMENT" });
+}
+
+// Our watcher Saga: spawn a new incrementAsync task on
+// each INCREMENT_ASYNC
+export function* watchIncrementAsync() {
+  // takeEvery is a helper function provided by redux-saga,
+  // to listen for dispatched INCREMENT_ASYNC actions
+  // and run incrementAsync each time.
+  yield takeEvery("INCREMENT_ASYNC", incrementAsync);
+}
+
+// notice how we now only export the rootSaga
+// single entry point to start all Sagas at once
+export default function* rootSaga() {
+  yield all([helloSaga(), watchIncrementAsync()]);
 }
