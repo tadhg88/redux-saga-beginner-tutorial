b7c765c323c603455f4f89b18110ba320270f840
Tim Whelan <twhelan@dr.local>
2020-06-04 17:29:38 +0100

    updating how delay method is called in incrementAsync method as outlined in "Making our code testable" section of tutorial.
    Instead of doing yield delay(1000), we're now doing yield call(delay, 1000). What's the difference?
    
    In the first case, the yield expression delay(1000) is evaluated before it gets passed to the caller
    In the second case, the yield expression call(delay, 1000) is what gets passed to the caller of next. call just like put, returns an Effect which instructs the middleware to call a given function with the given arguments.

--- sagas.js
+++ sagas.js
@@ -1,4 +1,4 @@
-import { put, takeEvery } from "redux-saga/effects";
+import { put, takeEvery, all, call } from "redux-saga/effects";
 
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
@@ -8,7 +8,7 @@ function* helloSaga() {
 
 // Our worker Saga: will perform the async increment task
 export function* incrementAsync() {
-  yield delay(1000);
+  yield call(delay, 1000);
 
   // instructs the middleware to schedule
   // the dispatching of an action to the store.
