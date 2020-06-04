c6356c7d94f9bc4d1f05b19b339130a323378d2f
Tim Whelan <twhelan@dr.local>
2020-06-04 12:24:06 +0100

    provide an additional button and a callback onIncrementAsync to the UI component.

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ function render() {
       value={store.getState()}
       onIncrement={() => action("INCREMENT")}
       onDecrement={() => action("DECREMENT")}
+      onIncrementAsync={() => action("INCREMENT_ASYNC")}
     />,
     document.getElementById("root")
   );
